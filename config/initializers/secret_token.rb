9d904205661dd12f5015ef33705fde8c838abaf7
tvirulha <tamanon.v@gmail.com>
2013-08-01 23:58:54 -0600

    Create new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VipBite::Application.config.secret_key_base = '5af9cdf155d2c0c59221be89a19e6de09391f3be16870df51409ea0044c031a370aae7f5a61a9af4348140a9a7cc341f90ce01ac9fb79226933434ccb659c031'
