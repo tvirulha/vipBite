a028d96992237af4fcc3f825f6a3ae3c12657a3e
tvirulha <tamanon.v@gmail.com>
2013-08-03 14:16:28 -0600

    Update index page.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def vipBite_Index
+	@title = 'Home';
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def siteTitle
+		result = '';
+
+		if @title.nil?
+			result = 'Home';
+		else
+			result = "#{@title}"
+		end
+	end
 end
